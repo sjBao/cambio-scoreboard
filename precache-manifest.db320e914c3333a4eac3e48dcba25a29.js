552e3d9f0bfd9bedc7255405892bbab13b0a1313
= <=>
2019-09-18 12:55:26 -0400

    Updates

---
+++ precache-manifest.db320e914c3333a4eac3e48dcba25a29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d8c64b2e47df4925212e5cacadba49",
+    "url": "%20//sjbao.github.io/cambio-scoreboard/index.html"
+  },
+  {
+    "revision": "9872cb574692a6ea484d",
+    "url": "%20//sjbao.github.io/cambio-scoreboard/static/css/main.6bcc434b.chunk.css"
+  },
+  {
+    "revision": "7209fe43532ef2edaecf",
+    "url": "%20//sjbao.github.io/cambio-scoreboard/static/js/2.9d8c1107.chunk.js"
+  },
+  {
+    "revision": "9872cb574692a6ea484d",
+    "url": "%20//sjbao.github.io/cambio-scoreboard/static/js/main.66c8e889.chunk.js"
+  },
+  {
+    "revision": "7dd6d42f41b76ca50ea2",
+    "url": "%20//sjbao.github.io/cambio-scoreboard/static/js/runtime~main.648328e3.js"
+  }
+]);
