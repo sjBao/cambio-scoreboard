ccc724cf6a792b9c485c5df4e5d02ba756ab72f3
sjbao <supuilam@gmail.com>
2023-02-17 00:07:12 -0500

    Add link to create game page

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -1,18 +1,22 @@
+import { useNavigate } from "@solidjs/router";
 import type { Component } from "solid-js";
 import { Button } from "../../components/Button";
 import { Logo } from "../../components/Logo";
 import { SPLASH_PAGE_CONTENT } from "./copy";
 import styles from './Splash.module.css';
 
-export const Splash: Component= () => {
+export const Splash: Component = () => {
+
+	const navigate = useNavigate();
+
 	return (
 		<main class={styles.pageContainer}>
 			<Logo />
-			<Button label="New Game" />
+			<Button label="New Game" onClick={() => navigate("/cambio-scoreboard/scoreboard")} />
 			<article class={styles.rulesContainer}>
 				{SPLASH_PAGE_CONTENT.sections.map((content) => <section>
-						<h3>{content.title}</h3>
-						<div>{content.body}</div>
+					<h3>{content.title}</h3>
+					<div>{content.body}</div>
 				</section>)}
 			</article>
 		</main>
