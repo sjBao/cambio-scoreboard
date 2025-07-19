fc68cb5e9d8d2693676e567f02b2985098e75814
Tony Lam <supuilam@gmail.com>
2025-07-19 01:58:35 -0400

    restart

--- src/components/Logo/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { Component } from "solid-js";
-
-import styles from './Logo.module.css'
-
-export const Logo: Component = () => {
-    return (
-        <h1 class={styles.logo}>Cambio</h1>
-    )
-}
--- src/pages/Scoreboard/PlayerScores.tsx
+++
@@ -1,9 +0,0 @@
-import type { Component } from "solid-js";
-
-export const PlayerScores: Component = () => {
-  return (
-    <div>
-      <h1>PlayerScores</h1>
-    </div>
-  )
-}
