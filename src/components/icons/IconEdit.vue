d9976bc14c87b2d70258bac7652614269ca84613
Tony Lam <supuilam@gmail.com>
2025-07-21 12:38:09 -0400

    make edit icon, remove unused icons

---
+++ src/components/icons/IconEdit.vue
@@ -0,0 +1,19 @@
+<!-- Edit icon from Material Design Icons, distributed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0) license-->
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    role="img"
+    class="iconify iconify--mdi"
+    width="24"
+    height="24"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
+      fill="currentColor"
+    ></path>
+  </svg>
+</template>
